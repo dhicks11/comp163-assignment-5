742462d7097ce3cc5a05e223389200b9e11c1674
dhicks11 <daylenhicks10@gmail.com>
2025-10-03 03:41:02 -0400

    Challenge 1: Collatz sequence - while loop for unknown iterations

---
+++ daylenhicks_assignment_5.py
@@ -0,0 +1,17 @@
+print("=== Challenge 1: Collatz Conjecture ===")
+current_number = int(input("Enter starting number: "))
+step_count = 0
+
+print("Sequence:", current_number, end=" ")
+
+while current_number != 1:
+    if current_number % 2 == 0:
+        current_number = current_number // 2
+    else:
+        current_number = 3 * current_number + 1
+
+    print(current_number, end=" ")
+    step_count += 1  # counts steps
+
+print("\nSteps:", step_count)
+
